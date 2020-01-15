74eadb790005d4ff5326e4a2316c7d6d8d2bdfe7
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-01-15 08:34:06 +0100

    add boilderplate and qtdocument module

---
+++ livelexqt/pkginfo.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the livelex-qt Python package.
+#
+# Copyright © 2019 by Wilbert Berendsen <info@wilbertberendsen.nl>
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+Meta-information about the livelex-qt package.
+
+This information is used by the install script, and can be queried
+from other applications.
+
+"""
+
+import collections
+Version = collections.namedtuple("Version", "major minor patch")
+
+
+
+#: name of the package
+name = "livelex-qt"
+
+#: the current version
+version = Version(0, 1, 0)
+version_suffix = ""
+version_string = "{}.{}.{}".format(*version) + version_suffix
+
+#: short description
+description = "The livelexqt Python module"
+
+#: long description
+long_description = \
+    "Small module providing livelex parsing and highlighting for Qt"
+
+#: maintainer name
+maintainer = "Wilbert Berendsen"
+
+#: maintainer email
+maintainer_email = "info@wilbertberendsen.nl"
+
+#: homepage
+url = "https://github.com/wbsoft/livelex-qt"
+
+#: license
+license = "GPL"
+
