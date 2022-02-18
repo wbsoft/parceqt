f62cb8ec1d5b8b916f51d7d0ef57ad3ce779e756
Wilbert Berendsen <wbsoft@xs4all.nl>
2022-02-18 18:31:15 +0100

    add languages to View menu in debugger

--- parceqt/debug.py
+++ parceqt/debug.py
@@ -191,6 +191,7 @@ class DebugWindow(QMainWindow):
     def set_root_lexicon(self, lexicon):
         """Set the root lexicon to use."""
         self.lexiconChooser.set_root_lexicon(lexicon)
+        self._actions.set_root_lexicon(lexicon)
 
     def guess_root_lexicon(self):
         """Again choose the root lexicon based on the text."""
@@ -288,6 +289,7 @@ class DebugWindow(QMainWindow):
     def slot_root_lexicon_changed(self, lexicon):
         """Called when the root lexicon is changed."""
         parceqt.set_root_lexicon(self.document, lexicon)
+        self._actions.set_root_lexicon(lexicon)
 
     def highlight_current_line(self):
         """Highlight the current line."""
@@ -431,6 +433,7 @@ class Actions:
         self.view_tree_expand = QAction()
         self.view_tree_collapse = QAction()
         self.view_updated_region = QAction(checkable=True)
+        # themes
         self.view_theme = QAction()
         self.view_theme_reload = QAction()
         m = QMenu()
@@ -447,6 +450,32 @@ class Actions:
             m.addAction(a)
         m.addSeparator()
         m.addAction(self.view_theme_reload)
+        # languages
+        self.view_language = QAction()
+        self.view_language_guess = QAction()
+        m = QMenu()
+        self.view_language.setMenu(m)
+        g = self.view_language_actiongroup = QActionGroup(None)
+        a = QAction(g, checkable=True)
+        a.setText("&None")
+        a.setObjectName("None")
+        m.addAction(a)
+        m.addSeparator()
+        reg = parce.registry.registry.by_section()
+        for sect in sorted(reg):
+            if sect:
+                submenu = QMenu()
+                a = QAction(sect, m)
+                a.setMenu(submenu)
+                m.addAction(a)
+                entries = sorted(("{} ({})".format(entry.desc, entry.name), qualname) for qualname, entry in reg[sect].items())
+                for name, qualname in entries:
+                    a = QAction(g, checkable=True)
+                    a.setObjectName(qualname)
+                    a.setText(name)
+                    submenu.addAction(a)
+        m.addSeparator()
+        m.addAction(self.view_language_guess)
 
     def set_action_texts(self):
         self.file_open.setText("&Open File...")
@@ -457,6 +486,8 @@ class Actions:
         self.view_updated_region.setText("Show &Updated Region")
         self.view_theme.setText("T&heme")
         self.view_theme_reload.setText("&Reload Theme")
+        self.view_language.setText("&Language")
+        self.view_language_guess.setText("&Guess")
 
     def set_action_shortcuts(self):
         self.file_open.setShortcut(QKeySequence("Ctrl+O"))
@@ -479,6 +510,7 @@ class Actions:
         editmenu.addAction(self.edit_copy_html)
         viewmenu = QMenu("&View", menubar)
         viewmenu.addAction(self.view_theme)
+        viewmenu.addAction(self.view_language)
         viewmenu.addSeparator()
         viewmenu.addAction(self.view_tree)
         viewmenu.addAction(self.view_tree_expand)
@@ -498,6 +530,8 @@ class Actions:
         self.view_updated_region.triggered.connect(self.toggle_updated_region_visibility)
         self.view_theme_actiongroup.triggered.connect(self.slot_view_theme_selected)
         self.view_theme_reload.triggered.connect(self.reload_theme)
+        self.view_language_guess.triggered.connect(self.mainwindow.guess_root_lexicon)
+        self.view_language_actiongroup.triggered.connect(self.slot_view_language_selected)
 
     def open_file(self):
         """Implementation of Open File action."""
@@ -527,6 +561,15 @@ class Actions:
             theme = action.text()
         self.mainwindow.set_theme(theme)
 
+    def slot_view_language_selected(self, action):
+        """Switch to the selected language."""
+        if action.objectName() == "None":
+            lexicon = None
+        else:
+            r = parce.registry.registry
+            lexicon = r.lexicon(action.objectName())
+        self.mainwindow.set_root_lexicon(lexicon)
+
     def tree_expand_all(self):
         """Implementation of Expand All action."""
         self.mainwindow.treeView.expandAll()
@@ -540,6 +583,21 @@ class Actions:
         action = self.view_theme_actiongroup.checkedAction()
         if action:
             self.slot_view_theme_selected(action)
+
+    def set_root_lexicon(self, lexicon):
+        """Select the corresponding action in the language menu."""
+        for a in self.view_language_actiongroup.actions():
+            if lexicon:
+                if lexicon.qualname != a.objectName():
+                    continue
+            elif a.objectName() != "None":
+                continue
+            a.setChecked(True)
+            return
+        # no menu entry for this lexicon
+        a = self.view_language_actiongroup.checkedAction()
+        if a:
+            a.setChecked(False)
 
 
 class ExtraSelectionManager(QObject):
